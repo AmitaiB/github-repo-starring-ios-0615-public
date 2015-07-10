4d31ac4b847277d587363a851fcecb3466d0365d
AmitaiB <blickstein@gmail.com>
2015-07-09 22:42:03 -0400

    adds checkIfStarred, bad impl

--- github-repo-starring/FISGithubAPIClient.m
+++ github-repo-starring/FISGithubAPIClient.m
@@ -8,6 +8,8 @@
 
 #import "FISGithubAPIClient.h"
 #import "FISConstants.h"
+#import "FISReposDataStore.h"
+//#import "FISGithubRepository.h"
 #import <AFNetworking.h>
 
 @implementation FISGithubAPIClient
@@ -19,11 +21,36 @@ NSString *const GITHUB_API_URL=@"https://api.github.com";
 
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
 
-    [manager GET:githubURL parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
-        completionBlock(responseObject);
-    } failure:^(NSURLSessionDataTask *task, NSError *error) {
-        NSLog(@"Fail: %@",error.localizedDescription);
-    }];
+    [manager GET:githubURL 
+      parameters:nil
+         success:^(NSURLSessionDataTask *task, id responseObject)   {completionBlock(responseObject);}
+         failure:^(NSURLSessionDataTask *task, NSError *error)      {NSLog(@"Fail: %@",error.localizedDescription);}
+     ];
 }
+
+
+-(BOOL)checkIfStarred:(NSString *)fullName {
+        //This returns repos because of the URL hard-coded into githubURL (above) "/repositories?..."
+    FISReposDataStore *reposDataStore = [FISReposDataStore sharedDataStore];
+    
+    [FISGithubAPIClient getRepositoriesWithCompletion:^(NSArray *repoDictionaries) {
+        reposDataStore.repositories = [repoDictionaries mutableCopy];
+    }];
+    
+    
+    
+    
+//    FISGithubRepository *namedRepo = [[FISGithubRepository alloc] init];
+    __block BOOL isStarred;
+    [reposDataStore.repositories enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSString *fullNameFromURL = obj.fullName;
+        if ([fullName isEqualToString: fullNameFromURL]) {
+            
+        }
+    }];
+    
+}
+
+
 
 @end
